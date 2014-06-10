3c2780486505de491fd68860dd12450cf372ab69
Doug Johnston <dbjohnston@gmail.com>
2014-06-10 11:45:08 -0500

    Make all tests pass

--- test/unit/app/controllers/main.spec.js
+++ test/unit/app/controllers/main.spec.js
@@ -1,16 +1,24 @@
 'use strict';
 
 describe('DrumMachineCtrl', function() {
-  var scope, timeout, ctrl;
+  var scope, ctrl, drumMachine;
 
-  beforeEach(module('AngularDrumMachine'));
+  beforeEach(function() {
+    module('AngularDrumMachine');
 
-  beforeEach(inject(function($rootScope, $injector, $controller) {
-    scope = $rootScope.$new();
-    ctrl = $controller('DrumMachineCtrl', {$scope: scope});
-  }));
+    inject(function($rootScope, $injector, $controller) {
+      scope = $rootScope.$new();
+      ctrl = $controller('DrumMachineCtrl', {$scope: scope});
+    });
+
+    inject(function(_drumMachine_) {
+      drumMachine = _drumMachine_;
+      scope.machine = drumMachine;
+    });
+  });
 
   it('should set up a drum machine service', function() {
+    expect(drumMachine).toEqual(jasmine.any(Object));
     expect(scope.machine).toEqual(jasmine.any(Object));
   });
 
@@ -53,6 +61,7 @@ describe('DrumMachineCtrl', function() {
     });
 
     it('should tell the machine to update the tempo', function() {
+      scope.tempo = 120;
       scope.updateTempo();
       expect(scope.machine.setTempo).toHaveBeenCalledWith(120);
     });
