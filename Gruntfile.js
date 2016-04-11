8cb8a1556164e176c925da57c459b1e71d51e3de
RRadix <benjamin.horstman123@gmail.com>
2016-04-11 16:25:09 -0500

    Bug fix and click to edit for BPM
    I have fixed a bug that allowed user to press the play button multiple
    times causing multiple play loops. I have added a click to edit feature
    to the BPM readout.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,91 +1,91 @@
-module.exports = function (grunt) {
-    // Project configuration.
-    grunt.initConfig({
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
 
-        //Read the package.json (optional)
-        pkg: grunt.file.readJSON('package.json'),
+    //Read the package.json (optional)
+    pkg: grunt.file.readJSON('package.json'),
 
-        // Metadata.
-        meta: {
-            basePath: './',
-            srcPath: './public/sass/',
-            deployPath: './public/assets/css/',
-            bowerPath: './public/assets/bower_components/'
-        },
+    // Metadata.
+    meta: {
+        basePath: './',
+        srcPath: './public/sass/',
+        deployPath: './public/assets/css/',
+        bowerPath: './public/assets/bower_components/'
+    },
 
-        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-                '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-                '* Copyright (c) <%= grunt.template.today("yyyy") %> ',
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> ',
 
-        // Task configuration.
-        connect: {
-            server: {
-                options: {
-                    port: 8080,
-                    base: './public'
-                }
-            }
-        },
-
-        karma: {
-            unit: {
-                configFile: 'test/karma.conf.js',
-                autoWatch: false,
-                background: true
-            }
-        },
-
-        sass: {
-            dist: {
-                options: {
-                    style: 'compressed',
-                    loadPath: ['<%= meta.bowerPath %>foundation/scss/', '<%= meta.bowerPath %>foundation/scss/foundation/components/']
-                },
-                files: {
-                    '<%= meta.deployPath %>main.css': '<%= meta.srcPath %>main.scss'
-                }
-            }
-        },
-
-        uglify: {
-            options: {
-                mangle: false
-            },
-            my_target: {
-                files: {
-                    'public/assets/js/angular_drums.min.js': ['public/assets/bower_components/angular-route/angular-route.min.js', 'public/assets/bower_components/howler/howler.min.js', 'public/app/**/*.js']
-                }
-            }
-        },
-
-        watch: {
-            sasstocss: {
-                files: ['<%= meta.srcPath %>/**/*.scss'],
-                tasks: ['sass']
-            },
-
-            /* watch and see if our javascript files change, or new packages are installed */
-            js: {
-                files: ['public/app/**/*.js'],
-                tasks: ['uglify']
-            },
-
-            karma: {
-                files: ['public/app/**/*.js', 'test/**/*.js'],
-                tasks: ['karma:unit:run']
-            }
+    // Task configuration.
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: './public'
         }
+      }
+    },
 
-    });
+    karma: {
+      unit: {
+        configFile: 'test/karma.conf.js',
+        autoWatch: false,
+        background: true
+      }
+    },
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-karma');
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+          loadPath: ['<%= meta.bowerPath %>foundation/scss/', '<%= meta.bowerPath %>foundation/scss/foundation/components/']
+        },
+        files: {
+            '<%= meta.deployPath %>main.css': '<%= meta.srcPath %>main.scss'
+        }
+      }
+    },
 
-    // Default task.
-    grunt.registerTask('default', ['sass', 'uglify', 'connect', 'watch']);
-    grunt.registerTask('test', ['sass', 'uglify', 'karma:unit:start', 'connect', 'watch']);
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'public/assets/js/angular_drums.min.js': ['public/assets/bower_components/angular-route/angular-route.min.js', 'public/assets/bower_components/howler/howler.min.js', 'public/app/**/*.js']
+        }
+      }
+    },
+
+    watch: {
+      sasstocss: {
+        files: ['<%= meta.srcPath %>/**/*.scss'],
+        tasks: ['sass']
+      },
+
+      /* watch and see if our javascript files change, or new packages are installed */
+      js: {
+        files: ['public/app/**/*.js'],
+        tasks: ['uglify']
+      },
+
+      karma: {
+        files: ['public/app/**/*.js', 'test/**/*.js'],
+        tasks: ['karma:unit:run']
+      }
+    }
+
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-karma');
+
+  // Default task.
+  grunt.registerTask('default', ['sass', 'uglify', 'connect', 'watch']);
+  grunt.registerTask('test', ['sass', 'uglify', 'karma:unit:start', 'connect', 'watch']);
 };
--- public/app/services/drum_machine.js
+++ public/app/services/drum_machine.js
@@ -1,131 +1,131 @@
 'use strict';
 
 // drumMachine Model
-app.factory('drumMachine', function ($http, $q, timerQueue) {
-    // Private variables
-    var _playing = false;
-    var _currentBeat = 0;
-    var _delay = 100;
-    var _gridLength = 16;
-    var _tempo = 120;
-    var _timers = timerQueue;
-    var _rows = [];
+app.factory('drumMachine', function($http, $q, timerQueue) {
+  // Private variables
+  var _playing = false;
+  var _currentBeat = 0;
+  var _delay = 100;
+  var _gridLength = 16;
+  var _tempo = 120;
+  var _timers = timerQueue;
+  var _rows = [];
 
-    function loadInstruments(instrumentFile) {
-        var item, player, instrument;
-        var file = instrumentFile || "/app/services/data/instruments/kit-1.json";
+  function loadInstruments(instrumentFile) {
+    var item, player, instrument;
+    var file = instrumentFile || "/app/services/data/instruments/kit-1.json";
 
-        return $http.get(file).then(function (response) {
-            for (var i = 0; i < 4; i++) {
-                item = response.data.instruments[i];
-                player = new Howl({ urls: ["assets/audio/" + item.file] });
-                instrument = new Instrument(player, item);
+    return $http.get(file).then(function(response) {
+      for(var i = 0; i < 4; i++) {
+        item = response.data.instruments[i];
+        player = new Howl({ urls: ["assets/audio/" + item.file] });
+        instrument = new Instrument(player, item);
 
-                _rows.push(new Row(instrument, _gridLength));
-            }
-            return "Instruments Loaded";
-        });
-    }
+        _rows.push(new Row(instrument, _gridLength));
+      }
+      return "Instruments Loaded";
+    });
+  }
 
-    function loadSequence(sequenceFile) {
-        var file = sequenceFile || "/app/services/data/sequences/seq-1.json";
+  function loadSequence(sequenceFile) {
+    var file = sequenceFile || "/app/services/data/sequences/seq-1.json";
 
-        reset();
+    reset();
 
-        return $http.get(file).then(function (response) {
-            _gridLength = response.data.gridLength;
-            setTempo(response.data.tempo);
+    return $http.get(file).then(function(response) {
+      _gridLength = response.data.gridLength;
+      setTempo(response.data.tempo);
 
-            for (var i = 0; i < 4; i++) {
-                for (var j = 0; j < _gridLength; j++) {
-                    if (response.data.rows[i][j] === "1") {
-                        _rows[i].getBeats()[j].activate();
-                    } else {
-                        _rows[i].getBeats()[j].deactivate();
-                    }
-                }
-            }
-            return "Sequence Loaded";
-        });
-    }
-
-    function rows() {
-        return _rows;
-    }
-
-    function tempo() {
-        return _tempo;
-    }
-
-    function gridLength() {
-        return _gridLength;
-    }
-
-    function currentBeat() {
-        return _currentBeat;
-    }
-
-    function setTempo(newTempo) {
-        _tempo = newTempo;
-        _delay = beatDelay();
-    }
-
-    function play() {
-        _playing = true;
-        _timers.add(playBeat(), beatDelay());
-    }
-
-    function stop() {
-        _playing = false;
-        _timers.clear();
-    }
-
-    function reset() {
-        stop();
-        _currentBeat = 0;
-        resetAllRows();
-    }
-
-    // Benchmark Code
-    //var lastTime = new Date().getTime();
-    function playBeat() {
-        return function () {
-            //var thisTime = new Date().getTime();
-            //console.log("Delay: " + _delay + " Diff: " + (thisTime - lastTime));
-            //lastTime = thisTime;
-            if (_currentBeat >= _gridLength) {
-                _currentBeat = 0;
-            }
-
-            for (var i = 0; i < _rows.length; i++) {
-                _rows[i].playSound(_currentBeat);
-            }
-            _currentBeat += 1;
-            _timers.add(playBeat(), _delay);
-        };
-    }
-
-    function resetAllRows() {
-        for (var i = 0; i < _rows.length; i++) {
-            _rows[i].reset();
+      for(var i = 0; i < 4; i++) {
+        for(var j = 0; j < _gridLength; j++) {
+          if (response.data.rows[i][j] === "1") {
+            _rows[i].getBeats()[j].activate();
+          } else {
+            _rows[i].getBeats()[j].deactivate();
+          }
         }
-    }
+      }
+      return "Sequence Loaded";
+    });
+  }
 
-    function beatDelay() {
-        return (1000 / (_tempo * 2) * 60);
-    }
+  function rows() {
+    return _rows;
+  }
 
-    // Return public functions
-    return {
-        loadInstruments: loadInstruments,
-        loadSequence: loadSequence,
-        gridLength: gridLength,
-        currentBeat: currentBeat,
-        rows: rows,
-        tempo: tempo,
-        setTempo: setTempo,
-        play: play,
-        stop: stop,
-        reset: reset
+  function tempo() {
+    return _tempo;
+  }
+
+  function gridLength() {
+    return _gridLength;
+  }
+
+  function currentBeat() {
+    return _currentBeat;
+  }
+
+  function setTempo(newTempo) {
+    _tempo = newTempo;
+    _delay = beatDelay();
+  }
+
+  function play() {
+    _playing = true;
+    _timers.add(playBeat(), beatDelay());
+  }
+
+  function stop() {
+    _playing = false;
+    _timers.clear();
+  }
+
+  function reset() {
+    stop();
+    _currentBeat = 0;
+    resetAllRows();
+  }
+
+  // Benchmark Code
+  //var lastTime = new Date().getTime();
+  function playBeat() {
+    return function() {
+      //var thisTime = new Date().getTime();
+      //console.log("Delay: " + _delay + " Diff: " + (thisTime - lastTime));
+      //lastTime = thisTime;
+      if (_currentBeat >= _gridLength) {
+        _currentBeat = 0;
+      }
+
+      for (var i = 0; i < _rows.length; i++) {
+        _rows[i].playSound(_currentBeat);
+      }
+      _currentBeat += 1;
+      _timers.add(playBeat(), _delay);
+    };
+  }
+
+  function resetAllRows() {
+    for(var i = 0; i < _rows.length; i++) {
+      _rows[i].reset();
     }
+  }
+
+  function beatDelay() {
+    return (1000 / (_tempo * 2) * 60);
+  }
+
+  // Return public functions
+  return {
+    loadInstruments: loadInstruments,
+    loadSequence: loadSequence,
+    gridLength: gridLength,
+    currentBeat: currentBeat,
+    rows: rows,
+    tempo: tempo,
+    setTempo: setTempo,
+    play: play,
+    stop: stop,
+    reset: reset
+  }
 });
